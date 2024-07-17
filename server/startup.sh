5234e81923e3bc5df99a35e24f35b604720de305
André Luís <andreluismoura22@gmail.com>
2024-07-17 09:06:12 -0300

    mod: changes to app startup to try to make job work

--- server/startup.sh
+++ server/startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# this setup is necessary for cronjob and its env variables reading process
+env > /tmp/environment_tmp
+mv /tmp/environment_tmp /etc/environment
+
 # cron
 apt-get update -y && apt-get install -y cron
 echo "* * * * * cd /home/site/wwwroot && /usr/local/bin/php artisan schedule:run >> cron.log" | crontab -
